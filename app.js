78c1190f01150a678a87fc900598a2beeff77c36
OCGZszvMae <lcs930415@gmail.com>
2021-12-25 23:51:18 -0500

    initial push

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('hello from the middleware');
+  next();
+});
+
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tours', tourRouter);
+
+module.exports = app;
