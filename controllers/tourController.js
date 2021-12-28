9bccd6f245d05c4930590dd851b7702bbcd650cb
OCGZszvMae <lcs930415@gmail.com>
2021-12-28 00:03:06 -0500

    add pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -45,6 +45,17 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    //4) pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.quer.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     //execute query
     const tours = await query;
 
