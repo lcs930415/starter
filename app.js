8f34dfa47a9339f6c0e0f88bf0e121a837235e72
OCGZszvMae <lcs930415@gmail.com>
2021-12-29 20:57:37 -0500

    113. add global error handler

--- app.js
+++ app.js
@@ -18,10 +18,21 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
+
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: "Can't find the requested url",
+  const err = new Error('Cannot find the requested url');
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+//central error handler
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    stauts: err.status,
+    message: err.message,
   });
 });
 
