cb59fd7350a54e0c0c5309bc4af0fba8b5d3d143
OCGZszvMae <lcs930415@gmail.com>
2022-01-03 17:58:20 -0500

    add route for user sign up

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
+
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+
 const router = express.Router();
-const uesrController = require('../controllers/userController');
+
+router.post('/signup', authController.signup);
 
 router
   .route('/')
