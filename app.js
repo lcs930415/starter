62260d358a80066db231920c81be684c78895f7b
OCGZszvMae <lcs930415@gmail.com>
2022-01-13 23:57:47 -0500

    安装使用helmet来自动添加一系列可以提高安全性能的headers到response中。

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,7 +10,12 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-//Global Middlewares
+//1.Global Middlewares
+
+//set security http header
+app.use(helmet());
+
+//development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -22,7 +28,10 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+//body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//serving static files
 app.use(express.static('./public'));
 
 app.use((req, res, next) => {
