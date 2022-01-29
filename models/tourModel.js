464c136310504dbad1a39de55dfd314bebccb719
Chris Li <hfl@economical.com>
2022-01-29 01:46:23 -0500

    150:Embed guides into tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 /* eslint-disable prefer-arrow-callback */
 const mongoose = require('mongoose');
 const validator = require('validator');
+const User=require('./userModel')
 
 const tourSchema = new mongoose.Schema(
   {
@@ -11,7 +12,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, 'input tour name is too long>40'],
       minlength: [10, 'input tour name is too short<10'],
-      validate: [validator.isAlpha, ' Tour name should only contain alphabets'],
+      //validate: [validator.isAlpha, ' Tour name should only contain alphabets'],
     },
     duration: {
       type: Number,
@@ -69,6 +70,35 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour:{
+      type:Boolean,
+      default:false
+    },
+    startLocation:{
+      //GeoJson
+      type:{
+        type:String,
+        default:'Point',
+        enum:['Point']
+      },
+      coordinates:[Number],
+      address:String,
+      description:String
+    },
+    locations:[
+      {
+        type:{
+          type:String,
+          default:'Point',
+          enum:['Point'],
+          coordinates:[Number],
+          address:String,
+          description:String,
+          day:Number
+        }
+      }
+    ],
+    guides:Array
   },
   {
     toJSON: { virtuals: true },
@@ -87,13 +117,15 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
-tourSchema.pre('save', function (next) {
-  //console.log("will save document")
+tourSchema.pre('save', async function (next) {
+  const guidesPromises=this.guides.map(async id=>await User.findById(id))
+  this.guides=await Promise.all(guidesPromises);
   next();
 });
 
 tourSchema.post('save', function (doc, next) {
   //console.log("second middleware")
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
