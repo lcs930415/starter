4b97e98fc82af7b25a9dccc29dbac6dc028c3209
Chris Li <hfl@economical.com>
2022-01-30 16:52:51 -0500

    157. implement nested route. 例如POST tours/:tourId/reviews是为id为tourId的tour创建一个review，也即review的route被nested在了tour的route里面。

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 
 const router = express.Router();
 
@@ -29,5 +30,10 @@ router
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
   );
-
+router.route('/:tourId/reviews')
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
+  );
 module.exports = router;
